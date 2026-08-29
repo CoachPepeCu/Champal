49c320db2e9fbab41fb3b7b93b8cfc7b8e1befef
CoachPepeCu <cunninghamclick@gmail.com>
2026-08-29 09:33:02 -0600

    Retira secciones no utilizadas de la página principal

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,7 @@ import Hero from "@/components/Hero";
 import Bienvenidos from "@/components/Bienvenidos";
 import Comunidad from "@/components/Comunidad";
 import Niveles from "@/components/Niveles";
-import Ventajas from "@/components/Ventajas";
-import StatsRow from "@/components/StatsRow";
-import WhyChampal from "@/components/WhyChampal";
-import HistoryTimeline from "@/components/HistoryTimeline";
-import Testimonials from "@/components/Testimonials";
 import ExploreChampal from "@/components/ExploreChampal";
-import AdmissionsSteps from "@/components/AdmissionsSteps";
-import FinalCTA from "@/components/FinalCTA";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
 
@@ -23,14 +16,7 @@ export default function Home() {
         <Bienvenidos />
         <Comunidad />
         <Niveles />
-        <Ventajas />
-        <StatsRow />
-        <WhyChampal />
-        <HistoryTimeline />
-        <Testimonials />
         <ExploreChampal />
-        <AdmissionsSteps />
-        <FinalCTA />
       </main>
       <Footer />
       <WhatsAppButton />
